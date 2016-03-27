e9dd9087ea3fbc3d31accf3cadc1b309d2117f42
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2016-03-27 12:41:55 +0900

    update initialize process

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ grp2 = 26
 grp3 = 16
 grp4 = 22
 
-brightness0 = 1
+brightness0 = 0.5
 brightness1 = 400
 brightness2 = 700
 brightness3 = 1023
@@ -23,34 +23,59 @@ led_all = [grp1, grp2, grp3, grp4]
 
 
 def init():
+  print 'init'
+  GPIO.setwarnings(False)
   GPIO.setmode(GPIO.BOARD)
-
-  # pwm
   GPIO.setup(pwm, GPIO.OUT)
+  p = GPIO.PWM(pwm, brightness3)
+  p.start(100)
   GPIO.output(pwm, True)
-  GPIO.PWM(pwm, brightness0)
 
-  # led out
+  # グループ1からグループ4まで点灯
   for grp in led_all:
-    sleep(0.25)
+    print 'pin %d on' % grp
     GPIO.setup(grp, GPIO.OUT)
     GPIO.output(grp, True)
+    sleep(0.25)
 
-  # 全灯(1s)
-  wait = 1.0 / (brightness3 - brightness0)
-  for brightness in range(1, 1024):
-    GPIO.PWM(pwm, brightness)
-    sleep(wait)
+  # 点滅
+  for x in range(0, 2):
+    p.ChangeDutyCycle(0)
+    sleep(0.2)
+    p.ChangeDutyCycle(100)
+    sleep(0.2)
 
-  # グループ4から1まで順に消灯(250ms/group)
-  wait = 1.0 / 250
+  for b in range(100, -1, -1):
+    p.ChangeDutyCycle(b)
+    sleep(0.01)
+  for b in range(0, 101):
+    p.ChangeDutyCycle(b)
+    sleep(0.01)
+
+  # グループ4から1まで順に消灯
   for grp in [grp4, grp3, grp2, grp1]:
+    print 'pin %d off' % grp
     GPIO.output(grp, False)
-    sleep(wait)
+    sleep(0.25)
+  GPIO.output(pwm, False)
+
+  print 'initialize done'
+
+
+demo = False
 
 
 def main() :
   init()
+
+
+  while True:
+    break
+    # TODO デモモードの分岐
+    if demo:
+      pass
+    else:
+      pass
 
 
 if __name__ == '__main__':
